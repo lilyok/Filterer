e22e43d5ee31e0cdde59af459c086c62f8808660
lilyok <lilyoknabieva@gmail.com>
2015-12-07 14:43:47 +0300

    Initial Commit

---
+++ Filterer/CrazyFilter.swift
@@ -0,0 +1,172 @@
+//
+//  CrazyFilter.swift
+//  Filterer
+//
+//  Created by lilil on 05.12.15.
+//  Copyright © 2015 UofT. All rights reserved.
+//
+
+import UIKit
+
+class Filter {
+    var redCoeff: Float32
+    var greenCoeff: Float32
+    var blueCoeff: Float32
+    
+    
+    init(redCoeff: Float32 = 1, greenCoeff: Float32 = 1, blueCoeff: Float32 = 1) {
+        self.redCoeff = redCoeff
+        self.greenCoeff = greenCoeff
+        self.blueCoeff = blueCoeff
+    }
+    
+    
+    func filtered(image: UIImage)-> UIImage {
+        let rgbaImage = RGBAImage(image: image)!
+        for y in 0..<rgbaImage.height {
+            for x in 0..<rgbaImage.width {
+                let index = rgbaImage.width * y + x
+                var currentPixel = rgbaImage.pixels[index]
+                
+                currentPixel.red = UInt8(Float32(currentPixel.red) * redCoeff > 255 ? Float(255.0) : (Float32(currentPixel.red) * redCoeff))
+                currentPixel.green = UInt8((Float32(currentPixel.green) * greenCoeff) > 255 ? 255 : (Float32(currentPixel.green) * greenCoeff))
+                currentPixel.blue = UInt8((Float32(currentPixel.blue) * blueCoeff) > 255 ? 255 : (Float32(currentPixel.blue) * blueCoeff))
+                
+                rgbaImage.pixels[index] = currentPixel
+            }
+        }
+        
+        return rgbaImage.toUIImage()!
+    }
+    
+}
+
+class RotateColorFilter: Filter {
+    var addIndex: Int
+    init(nextColor: Float32 = 1) {
+        if (nextColor < 1.0 / 3.0) {
+            self.addIndex = 0
+        }
+        else if (nextColor > 2.0 / 3.0) {
+            self.addIndex = -1
+        }
+        else {
+            addIndex = 1
+        }
+        
+        super.init()
+    }
+    
+    override
+    func filtered(image: UIImage)-> UIImage {
+        let rgbaImage = RGBAImage(image: image)!
+        for y in 0..<rgbaImage.height {
+            for x in 0..<rgbaImage.width {
+                let index = rgbaImage.width * y + x
+                var currentPixel = rgbaImage.pixels[index]
+
+                if (addIndex == 1) {
+                    let blue = currentPixel.blue
+                    currentPixel.blue = currentPixel.green
+                    currentPixel.green = currentPixel.red
+                    currentPixel.red = blue
+                } else if (addIndex == -1) {
+                    let green = currentPixel.green
+                    currentPixel.green = currentPixel.blue
+                    currentPixel.blue = currentPixel.red
+                    currentPixel.red = green
+                } else {
+                    let blue = currentPixel.blue
+                    currentPixel.blue = currentPixel.red
+                    currentPixel.red = blue
+                }
+                
+                rgbaImage.pixels[index] = currentPixel
+            }
+        }
+        
+        return rgbaImage.toUIImage()!
+    }
+}
+
+class BlackAndWhiteFilter: Filter {
+    var coeff: Float32
+    init(commonCoeff: Float32 = 0) {
+        self.coeff = commonCoeff + 1
+        super.init()
+    }
+    
+    override
+    func filtered(image: UIImage)-> UIImage {
+        let rgbaImage = RGBAImage(image: image)!
+        for y in 0..<rgbaImage.height {
+            for x in 0..<rgbaImage.width {
+                let index = rgbaImage.width * y + x
+                var currentPixel = rgbaImage.pixels[index]
+                
+                if (UInt16(currentPixel.red) + UInt16(currentPixel.green) + UInt16(currentPixel.blue) > 381) {
+                    var color = Float32(currentPixel.red/3 + currentPixel.green/3 + currentPixel.blue/3) * coeff
+                    if (color > 255) {
+                        color = 255
+                    }
+
+                    currentPixel.red = UInt8(color)
+                    currentPixel.green = UInt8(color)
+                    currentPixel.blue = UInt8(color)
+                } else {
+                    var color = Float32(currentPixel.red/3 + currentPixel.green/3 + currentPixel.blue/3) / coeff
+                    if (color > 255) {
+                        color = 255
+                    }
+                    currentPixel.red = UInt8(color)
+                    currentPixel.green = UInt8(color)
+                    currentPixel.blue = UInt8(color)
+                }
+                
+                rgbaImage.pixels[index] = currentPixel
+            }
+        }
+        
+        return rgbaImage.toUIImage()!
+    }
+}
+
+
+class CrazyFilter {
+    let sourceImg: UIImage
+    var destinationImg: UIImage
+    var filters: [String: Filter]
+    init(image: UIImage, dictFilters: [String: Filter]){
+        sourceImg = image
+        destinationImg = sourceImg
+        filters = dictFilters
+    }
+    
+    func changeFilter(nameOfFilter: String, newFilter: Filter)-> Void {
+        if let _ = filters[nameOfFilter] {
+            filters[nameOfFilter] = newFilter
+        }
+    }
+    
+    
+    // You can to apply list of the filters
+    func applyFilters(namesOfFilters: [String])-> UIImage {
+        destinationImg = sourceImg
+        for name in namesOfFilters {
+            if let filter = filters[name] {
+                destinationImg = filter.filtered(destinationImg)
+            }
+        }
+        return destinationImg
+    }
+    
+    //you can apply one of the filters
+    func applyFilter(nameOfFilter: String)-> UIImage {
+        destinationImg = sourceImg
+        if let filter = filters[nameOfFilter] {
+            destinationImg = filter.filtered(destinationImg)
+        }
+        return destinationImg
+    }
+    
+}
---
+++ Filterer/ViewController.swift
@@ -0,0 +1,378 @@
+//
+//  ViewController.swift
+//  Filterer
+//
+//  Created by Jack on 2015-09-22.
+//  Copyright © 2015 UofT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var filteredImage: UIImage?
+    var imageProcessor: CrazyFilter!
+    var lastFilter = ""
+    let duration = 0.6
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet weak var secondImageView: UIImageView!
+
+    @IBOutlet var originalText: UILabel!
+    @IBOutlet var secondaryMenu: UIView!
+    @IBOutlet var bottomMenu: UIView!
+    
+    @IBOutlet weak var editFilter: UIButton!
+    @IBOutlet var filterButton: UIButton!
+    @IBOutlet var filterValue: UISlider!
+    
+    @IBOutlet var imageToggle: UIButton!
+    
+    @IBOutlet var btnBlackAndWhite: UIButton!
+    
+    @IBOutlet weak var btnHalfBrightness: UIButton!
+    
+    @IBOutlet weak var btnTwiceBrightness: UIButton!
+    
+    @IBOutlet weak var btnRotateColor: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = UIImage(named: "scenery")!
+        secondImageView.image = UIImage(named: "scenery")!
+        clearProperties()
+        let tapGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "touch:")
+        tapGestureRecognizer.minimumPressDuration = 0
+        imageView.userInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func clearProperties() {
+        secondImageView.alpha = 0
+
+        filterButton.selected = false
+        editFilter.selected = false
+        editFilter.enabled = false
+        editFilter.setTitle("Edit", forState: .Selected)
+        
+        imageToggle.setTitle("Compare", forState: .Selected)
+        imageToggle.selected = false
+        imageToggle.enabled = false
+        filterValue.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        filterValue.translatesAutoresizingMaskIntoConstraints = false
+        secondaryMenu.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
+        originalText.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        originalText.translatesAutoresizingMaskIntoConstraints = false
+        btnBlackAndWhite.titleLabel!.lineBreakMode = NSLineBreakMode.ByWordWrapping;
+        btnHalfBrightness.titleLabel!.lineBreakMode = NSLineBreakMode.ByWordWrapping;
+        btnTwiceBrightness.titleLabel!.lineBreakMode = NSLineBreakMode.ByWordWrapping;
+        btnRotateColor.titleLabel!.lineBreakMode = NSLineBreakMode.ByWordWrapping;
+        
+        
+        imageProcessor = CrazyFilter(image: imageView.image!,
+            dictFilters: ["infernalFilter": Filter(redCoeff: 1.0, greenCoeff: 2, blueCoeff: 2),
+                "moreRedFilter": Filter(redCoeff: 2),
+                "twiceBrightnessFilter": Filter(redCoeff: 2, greenCoeff: 2, blueCoeff: 2),
+                "blackAndWhiteFilter": BlackAndWhiteFilter(commonCoeff: 0.5),
+                "rotateColorFilter": RotateColorFilter()])
+       
+    }
+    
+    @IBAction func touch(sender: AnyObject) {
+        if(sender.state == UIGestureRecognizerState.Began){
+            onImageToggle(sender)
+        }
+        if(sender.state == UIGestureRecognizerState.Ended){
+            onImageToggle(sender)
+        }
+    }
+    
+
+
+    // MARK: Share
+    @IBAction func onShare(sender: AnyObject) {
+        if imageToggle.selected {
+            crossFadeImage(true)
+            hideFlowMenu(self.originalText)
+            imageToggle.selected = false
+        }
+        editFilter.selected = false
+        filterButton.selected = false
+        hideSecondaryMenu()
+        hideFlowMenu(filterValue)
+        let activityController = UIActivityViewController(activityItems: ["Check out our really cool app", secondImageView.image!], applicationActivities: nil)
+        presentViewController(activityController, animated: true, completion: nil)
+    }
+    
+    // MARK: New Photo
+    @IBAction func onNewPhoto(sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "New Photo", message: nil, preferredStyle: .ActionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .Default, handler: { action in
+            self.showCamera()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Album", style: .Default, handler: { action in
+            self.showAlbum()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+    }
+    
+    func showCamera() {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .Camera
+        
+        presentViewController(cameraPicker, animated: true, completion: nil)
+    }
+    
+    func showAlbum() {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .PhotoLibrary
+        
+        presentViewController(cameraPicker, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        dismissViewControllerAnimated(true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            secondImageView.image = image
+            filteredImage = nil
+            if imageToggle.selected {
+                crossFadeImage(true)
+                hideFlowMenu(self.originalText)
+                imageToggle.selected = false
+            }
+            clearProperties()
+            hideSecondaryMenu()
+            hideFlowMenu(filterValue)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: Filter Menu
+    @IBAction func onFilter(sender: UIButton) {
+        if (sender.selected) {
+            hideSecondaryMenu()
+            sender.selected = false
+        } else {
+            if imageToggle.selected {
+                crossFadeImage(true)
+                hideFlowMenu(self.originalText)
+                imageToggle.selected = false
+            }
+            
+            hideFlowMenu(filterValue)
+            editFilter.selected = false
+            
+            if (editFilter.selected) {
+                onFilterEdit(sender)
+            }
+            showSecondaryMenu()
+            sender.selected = true
+        }
+    }
+    
+    
+    @IBAction func onChangedFilterValue(sender: UISlider) {
+        let value = Float(sender.value)
+        var newFilter: Filter!
+        switch lastFilter {
+            case "infernalFilter":
+                newFilter = Filter(redCoeff: 1.0, greenCoeff: 2*2*value, blueCoeff: 2*2*value)
+            case "moreRedFilter":
+                newFilter = Filter(redCoeff: 2*2*value, greenCoeff: value)
+            case "twiceBrightnessFilter":
+                newFilter = Filter(redCoeff: 2*2*value, greenCoeff: 2*2*value, blueCoeff: 2*2*value)
+            case "blackAndWhiteFilter":
+                newFilter = BlackAndWhiteFilter(commonCoeff: value)
+            case "rotateColorFilter":
+                newFilter = RotateColorFilter(nextColor: value)
+            default:
+                return
+        }
+        imageProcessor.changeFilter(lastFilter, newFilter: newFilter)
+        secondImageView.image = imageProcessor.applyFilter(lastFilter)
+        
+    }
+    
+    
+    @IBAction func onFilterEdit(sender: AnyObject) {
+        editFilter.selected = !editFilter.selected
+        if editFilter.selected {
+            if filterButton.selected {
+                onFilter(filterButton)
+            }
+            if imageToggle.selected {
+                crossFadeImage(true)
+                hideFlowMenu(self.originalText)
+                imageToggle.selected = false
+            }
+            let bottomConstraint = filterValue.bottomAnchor.constraintEqualToAnchor(bottomMenu.topAnchor)
+            let leftConstraint = filterValue.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
+            let rightConstraint = filterValue.rightAnchor.constraintEqualToAnchor(view.rightAnchor)
+            
+            let heightConstraint = filterValue.heightAnchor.constraintEqualToConstant(44)
+            
+            showFlowMenu(view, childView: filterValue, bottomConstraint: bottomConstraint, leftConstraint: leftConstraint, rightConstraint: rightConstraint, heightConstraint: heightConstraint, finishAlpha: 0.75)
+            
+        } else {
+            hideFlowMenu(filterValue)
+        }
+    }
+    
+    @IBAction func onImageToggle(sender: AnyObject) {
+        guard (filteredImage != nil) else {
+            return
+        }
+        editFilter.selected = false
+        hideFlowMenu(filterValue)
+        imageToggle.selected = !imageToggle.selected
+        if imageToggle.selected {
+            hideSecondaryMenu()
+            filterButton.selected = false
+            hideFlowMenu(filterValue)
+            editFilter.selected = false
+            
+            
+            let heightConstraint = originalText.heightAnchor.constraintEqualToConstant(44)
+            let bottomConstraint = originalText.bottomAnchor.constraintEqualToAnchor(imageView.topAnchor, constant: 44.0)
+            let leftConstraint = originalText.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
+            let rightConstraint = originalText.rightAnchor.constraintEqualToAnchor(view.rightAnchor)
+            
+            crossFadeImage(false)
+            
+            showFlowMenu(imageView, childView: originalText, bottomConstraint: bottomConstraint, leftConstraint: leftConstraint, rightConstraint: rightConstraint, heightConstraint: heightConstraint, finishAlpha: 0.75)
+            
+        } else {
+            crossFadeImage(true)
+            
+            hideFlowMenu(self.originalText)
+        }
+    }
+    
+    @IBAction func rotateColor(sender: AnyObject) {
+        filterValue.setValue(0.5, animated: true)
+        imageProcessor.changeFilter("rotateColorFilter", newFilter: RotateColorFilter())
+        filteredImage = imageProcessor.applyFilter("rotateColorFilter")
+        lastFilter = "rotateColorFilter"
+        imageToggle.enabled = true
+        editFilter.enabled = true
+
+        crossFadeImage(true)
+
+    }
+    
+    @IBAction func setTwiceBrightness(sender: AnyObject) {
+        filterValue.setValue(0.5, animated: true)
+        imageProcessor.changeFilter("twiceBrightnessFilter", newFilter: Filter(redCoeff: 2, greenCoeff: 2, blueCoeff: 2))
+        filteredImage = imageProcessor.applyFilter("twiceBrightnessFilter")
+        lastFilter = "twiceBrightnessFilter"
+        imageToggle.enabled = true
+        editFilter.enabled = true
+
+        crossFadeImage(true)
+    }
+    
+    @IBAction func setHalfBrightness(sender: AnyObject) {
+        filterValue.setValue(0.5, animated: true)
+        imageProcessor.changeFilter("infernalFilter", newFilter: Filter(redCoeff: 1.0, greenCoeff: 2, blueCoeff: 2))
+        filteredImage = imageProcessor.applyFilter("infernalFilter")
+        lastFilter = "infernalFilter"
+        imageToggle.enabled = true
+        editFilter.enabled = true
+
+        crossFadeImage(true)
+    }
+    
+    @IBAction func setBlackAndWhite(sender: AnyObject) {
+        filterValue.setValue(0.5, animated: true)
+        imageProcessor.changeFilter("blackAndWhiteFilter", newFilter: BlackAndWhiteFilter(commonCoeff: 0.5))
+        filteredImage = imageProcessor.applyFilter("blackAndWhiteFilter")
+        lastFilter = "blackAndWhiteFilter"
+        imageToggle.enabled = true
+        editFilter.enabled = true
+
+        crossFadeImage(true)
+    }
+    
+    @IBAction func redIt(sender: AnyObject) {
+        filterValue.setValue(0.5, animated: true)
+        imageProcessor.changeFilter("moreRedFilter", newFilter: Filter(redCoeff: 2))
+        filteredImage = imageProcessor.applyFilter("moreRedFilter")
+        lastFilter = "moreRedFilter"
+        imageToggle.enabled = true
+        editFilter.enabled = true
+
+        crossFadeImage(true)
+    }
+
+    func showSecondaryMenu() {
+        editFilter.selected = false
+        hideFlowMenu(filterValue)
+        let bottomConstraint = secondaryMenu.bottomAnchor.constraintEqualToAnchor(bottomMenu.topAnchor)
+        let leftConstraint = secondaryMenu.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
+        let rightConstraint = secondaryMenu.rightAnchor.constraintEqualToAnchor(view.rightAnchor)
+        
+        let heightConstraint = secondaryMenu.heightAnchor.constraintEqualToConstant(44)
+        
+        showFlowMenu(view, childView: secondaryMenu, bottomConstraint: bottomConstraint, leftConstraint: leftConstraint, rightConstraint: rightConstraint, heightConstraint: heightConstraint, finishAlpha: 0.75)
+    }
+
+    func hideSecondaryMenu() {
+        hideFlowMenu(self.secondaryMenu)
+    }
+    
+    func hideFlowMenu(childView: UIView) {
+        UIView.animateWithDuration(duration, animations: {
+            childView.alpha = 0
+            }) { completed in
+                if completed == true {
+                    childView.removeFromSuperview()
+                }
+        }
+    }
+    
+    func showFlowMenu(parentView: UIView, childView: UIView, bottomConstraint: NSLayoutConstraint, leftConstraint: NSLayoutConstraint, rightConstraint: NSLayoutConstraint, heightConstraint: NSLayoutConstraint, finishAlpha: CGFloat) {
+        parentView.addSubview(childView)
+        
+        NSLayoutConstraint.activateConstraints([bottomConstraint, leftConstraint, rightConstraint, heightConstraint])
+        
+        parentView.layoutIfNeeded()
+        
+        childView.backgroundColor = UIColor.blackColor()
+        childView.alpha = 0
+        UIView.animateWithDuration(duration) {
+            childView.alpha = finishAlpha
+        }
+        
+    }
+    
+    func crossFadeImage(is_show: Bool) {
+        if (is_show) {
+            secondImageView.alpha = 0
+            secondImageView.image = filteredImage
+            UIView.animateWithDuration(duration) {
+                self.secondImageView.alpha = 1.0
+            }
+        } else {
+            secondImageView.alpha = 1.0
+            UIView.animateWithDuration(duration) {
+                self.secondImageView.alpha = 0.0
+            }
+        }
+        
+    }
+
+}
+
